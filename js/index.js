0ea21805d53f799507a5229848596c63a50a48e0
LingyunG <fn121769102@gmail.com>
2016-05-26 19:23:58 -0400

    Now snake can move around.

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
         fillGrid: fillGrid
     };
     var timer = {
-        timeInterval: 600,
+        timeInterval: 300,
         run: function () {
             this.interval = setInterval(moveSnake, this.timeInterval);
         },
@@ -36,23 +36,26 @@ $(document).ready(function () {
         }
     };
     inGame = false;
-    init(coordinate, snake);
-
-
+    start();
+    $(document).keydown(function (event) {
+        if (inGame === true) {
+            keyboardHandler(event.which);
+        }
+    });
 
     // Functions
     function init(coordinate, snake) {
-        coordinate.grid = new Array(V_GRID);
-        for (var i = 0; i < V_GRID; i++) {
-            coordinate.grid[i] = new Array(H_GRID);
-            if (i === 0 || i === V_GRID - 1) {
-                for (var j = 0; j < H_GRID; j++) {
+        coordinate.grid = new Array(H_GRID);
+        for (var i = 0; i < H_GRID; i++) {
+            coordinate.grid[i] = new Array(V_GRID);
+            if (i === 0 || i === H_GRID - 1) {
+                for (var j = 0; j < V_GRID; j++) {
                     coordinate.grid[i][j] = OCCUPANT.TAKEN;
                 }
             }
             else {
-                for (var j = 0; j < H_GRID; j++) {
-                    if (j === 0 || j === H_GRID - 1) {
+                for (var j = 0; j < V_GRID; j++) {
+                    if (j === 0 || j === V_GRID - 1) {
                         coordinate.grid[i][j] = OCCUPANT.TAKEN;
                     }
                     else {
@@ -66,35 +69,40 @@ $(document).ready(function () {
         snake.position.push(Position(3, 1));
         snake.position.push(Position(2, 1));
         snake.position.push(Position(1, 1));
-        snake.head = snake.position[0];
-        snake.tail = snake.position[snake.position.length - 1];
         snake.length = 4;
-        snake.direction = { head: "+x", tail: "+x" };
-        coordinate.fillGrid(snake);
-        drawSnake(snake);
-    }
-
-    function fillGrid(snake) {
+        snake.direction = "+x";
         for (var i = 0; i < snake.position.length; i++) {
             var pos = snake.position[i];
-            if (0 < pos.x < H_GRID && 0 < pos.y < V_GRID) {
-                this.grid[pos.y][pos.x] = OCCUPANT.TAKEN;
+            if (0 < pos.x < V_GRID && 0 < pos.y < H_GRID) {
+                coordinate.grid[pos.x][pos.y] = OCCUPANT.TAKEN;
             }
             else {
                 throw (new Error("Invalid coordinate position."));
             }
         }
-        return;
+        drawSnake(snake);
+    }
+
+    function fillGrid(snake) {
+        var clearPos = snake.position[snake.position.length - 1];
+        var newPos = snake.position[0];
+        if (0 < clearPos.x < V_GRID && 0 < clearPos.y < H_GRID) {
+            this.grid[clearPos.x][clearPos.y] = OCCUPANT.SPACE;
+        }
+        if (0 < newPos.x < V_GRID && 0 < newPos.y < H_GRID) {
+            this.grid[newPos.x][newPos.y] = OCCUPANT.TAKEN;
+        }
     }
 
     function start() {
         inGame = true;
         ctx.clearRect(0, 0, WIDTH, HEIGHT);
         init(coordinate, snake);
-        // timer.run();
+        timer.run();
     }
     function drawSnake(snake) {
-        drawHead(snake.direction.head, snake.head.x, snake.head.y);
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+        drawHead(snake.direction, snake.position[0].x, snake.position[0].y);
         for (var i = 1; i < snake.position.length; i++) {
             drawTail(snake.position[i].x, snake.position[i].y);
         }
@@ -106,6 +114,73 @@ $(document).ready(function () {
     function drawTail(x, y) {
         var img = document.getElementById("tail_icon");
         ctx.drawImage(img, (x - 1) * UNIT, (y - 1) * UNIT);
+    }
+
+    function moveSnake() {
+        var dir = snake.direction;
+        for (var i = snake.position.length - 1; i > 0; i--) {
+            snake.position[i] = JSON.parse(JSON.stringify(snake.position[i - 1]));
+        }
+        switch (dir) {
+            case "+x":
+                snake.position[0].x++;
+                break;
+            case "-x":
+                snake.position[0].x--;
+                break;
+            case "+y":
+                snake.position[0].y--;
+                break;
+            default:
+                snake.position[0].y++;
+                break;
+        }
+        var go = (coordinate.grid[x][y] !== OCCUPANT.TAKEN);
+        if (go) {
+            coordinate.fillGrid(snake);
+            drawSnake(snake);
+        }
+        else {
+            timer.stop(function () {
+                inGame = false;
+                console.log('game failed');
+            });
+        }
+    }
+    function turnSnake(newDir) {
+        snake.direction = newDir;
+        drawSnake(snake);
+    }
+    function keyboardHandler(key) {
+        var currentDir = snake.direction;
+        var newDir;
+        switch (key) {
+            case 1:
+            case 38:
+            case 269:
+                newDir = "+y";
+                break;
+            case 2:
+            case 40:
+            case 270:
+                newDir = "-y";
+                break;
+            case 3:
+            case 37:
+            case 271:
+                newDir = "-x";
+                break;
+            case 4:
+            case 39:
+            case 272:
+                newDir = "+x";
+                break;
+            default:
+                console.log("key not implemented yet");
+        }
+        if (newDir !== undefined && currentDir !== newDir && currentDir[1] !== newDir[1]) {
+            turnSnake(newDir);
+        }
     }
 });
 
